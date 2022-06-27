af2058d31f5621ca6eddc25f10550a967c3064b9
Colin Spence <udappia@gmail.com>
2022-06-27 12:48:04 -0600

    Resolves absolute positioning bug; adds new showcase elements;

--- src/components/WrappersHero.tsx
+++ src/components/WrappersHero.tsx
@@ -1,11 +1,13 @@
 import { Parallax } from 'react-scroll-parallax';
-import { Box, Grid, makeStyles, Typography, useTheme, Button } from '@material-ui/core';
+import { Box, BoxProps, Container, Grid, makeStyles, Typography, useTheme, Button } from '@material-ui/core';
 import {useState, useEffect} from 'react';
 import {  newListOfFeaturedQueries, writtenCopy } from './ContentfulFetcher';
 import { DemoFunctions } from './DemoFunctions';
 import { IDE } from './IDE';
 import { fetchWrappers }from './CMScontent';
 import KeyboardArrowRightOutlined from '@material-ui/icons/KeyboardArrowRightOutlined';
+import { polywrapPalette } from '../theme';
+import classnames from "classnames";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,6 +26,7 @@ const useStyles = makeStyles((theme) => ({
   },
   grid: {
     justifyContent: 'center',
+    marginTop: theme.spacing(6),
     position: 'relative',
     [theme.breakpoints.down('sm')]: {
       '& .MuiGrid-item': {
@@ -46,6 +49,44 @@ const useStyles = makeStyles((theme) => ({
   },
   description: {
     marginTop: 20,
+  },
+  wrapperSelection: {
+    backgroundColor: polywrapPalette.secondary[900],
+    border: `1px solid ${polywrapPalette.secondary[800]}`,
+    borderRadius: 16,
+    cursor: "pointer",
+    display: "flex",
+    fontWeight: 700,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    opacity: 0.7,
+    paddingX: theme.spacing(2),
+    textAlign: "center",
+    transition: "all 0.25s ease-in-out",
+    height: 120,
+    width: 120,
+    "&:hover": {
+      opacity: 1,
+      boxShadow: theme.shadows[16],
+    },
+    "&.is-active": {
+      borderColor: polywrapPalette.secondary[300],
+      boxShadow: theme.shadows[16],
+      opacity: 1,
+      ".wrapperIcon": {
+        color: polywrapPalette.secondary[300],
+      }
+    }
+  },
+  wrapperSelectionIcon: {
+    backgroundColor: "currentColor",
+    marginBottom: theme.spacing(1),
+    minHeight: theme.spacing(5),
+    width: theme.spacing(5),
+  },
+  wrapperCTAButton: {
+    marginTop: theme.spacing(2),
   },
   IDEWrapper: {
     position: 'relative',
@@ -134,131 +175,144 @@ export const FeaturedWrappersSection = () => {
 
   return (
     <Box position='relative' className={classes.root}>
-      <Parallax
-        y={[20, -35]}
-        disabled={window.innerWidth < theme.breakpoints.values.md}
-      >
 
+      <Container maxWidth="md">
+        {/* exciting title for the section */}
+        <Typography
+          variant='h3'
+          align="center"
+          color='textPrimary'
+          className={classes.title}
+        >
+          {aboutThisSection.title}
+        </Typography>
+
+        {/* description about the wrapper dev experience */}
+        <Typography
+          variant='body1'
+          align="center"
+          color='textSecondary'
+          className={classes.description}
+        >
+          {aboutThisSection.description  }
+        </Typography>
+      </Container>
+
+      <Box
+        display="flex"
+        flexWrap="wrap"
+        justifyContent={wrappersData?.length >= 5 ? "space-between" : "center"}
+        mt={6}
+      >
+        { wrappersData.map((wrapper: any, index: number) => {
+            return (
+              <Box
+                key={index}
+                ml={index !== 0 ? 2 : 0} mr={index !== wrappersData.length - 1 ? 2 : 0}
+              >
+                <Box className={classnames(classes.wrapperSelection, wrappersTransitionID === index && "is-active")}>
+                  <Box className={classes.wrapperSelectionIcon}/>
+                  {wrapper.wrapperName}
+                </Box>
+              </Box>
+            )
+          })
+        }
+      </Box>
       {/* The lines below are used to check
             1. that wrappersData exists
             2. maps all the data to render the component*/}
-      { wrappersData && 
+        { wrappersData && 
         wrappersData.map((wrapper: any, index: number) =>
-        
-
-          // This grid is for the entire featured wrapper component
-          <Grid
+          <Box 
             key={index}
-            container
-            spacing={10}
-            alignItems='flex-start'
-            className={classes.grid}
+            mb={24}
             style={{
-              opacity: wrappersTransitionID === index ? '100%': '0%',
-              // visibility: wrappersTransitionID === index ? 'initial': 'hidden',
-              zIndex : wrappersTransitionID === index ? 99: -1,
-              transition: "all 1s ease-in",
-              position: 'absolute'
+              display: wrappersTransitionID === index ? "block" : "none",
             }}
           >
-            {/* // this grid is used to showcase the IDE and the CMS card */}
-            <Grid item xs={12} md={6}>
-              <Box className={classes.IDEWrapper}>
+            <Grid
+              container
+              spacing={6}
+              alignItems='flex-start'
+              className={classes.grid}
+            >
+              {/* This section is used to display the name of the wrapper, a description of the wrapper,
+                  additional copy to generate engagement, and a CTA button that takes users to docs of the
+                  specified wrappert
+              */}
+              <Grid item xs={12} md={6}>
 
-                {/* Card section for listing names of new functions */}
-                <Box className={classes.demoFunctionWrapper}>
-                  <Parallax
-                    y={[140, -13]}
-                    disabled={window.innerWidth < theme.breakpoints.values.md}
+                {/* The name of the wrapper currently displayed */}
+                <Typography
+                  variant='h4'
+                  color='textPrimary'
+                  className={classes.title}
+                >
+                  {wrapper.wrapperName}
+                </Typography>
+
+                {/* Description of the wrapper being displayed */}
+                <Typography
+                  variant='body1'
+                  color='textSecondary'
+                  className={classes.description}
+                >
+                  {wrapper.description } 
+                </Typography>
+
+                {/* CTA to get people to use the specific wrapper */}
+                <Button
+                  // still dunno what to calll this
+                  className={classes.wrapperCTAButton}
+                  color='primary'
+                  href={wrapper.docsLink}
+                  target="_blank"
+                  rel="noredirect"
+                  endIcon={<KeyboardArrowRightOutlined />}
+                  type='submit'
+                  variant='contained'
                   >
-                    {/* TODO: later down the road, use this section to map all the name
-                        of the functions within the wrapper and display them on a card.
-                        Also consider a way of setting the active function on "accent",
-                        while the other ones not being displayed could look grey.
+                  wrap with it
+                </Button>
+              </Grid>
 
-                    <DemoFunctions 
-                      content={
-                          featuredQueries
-                        } 
-                    />
-                   */}
+              {/* // this grid is used to showcase the IDE and the CMS card */}
+              <Grid item xs={12} md={6}>
+                <Box className={classes.IDEWrapper}>
+
+                  {/* Card section for listing names of new functions */}
+                  <Box className={classes.demoFunctionWrapper}>
+                    <Parallax
+                      y={[140, -13]}
+                      disabled={window.innerWidth < theme.breakpoints.values.md}
+                    >
+                      {/* TODO: later down the road, use this section to map all the name
+                          of the functions within the wrapper and display them on a card.
+                          Also consider a way of setting the active function on "accent",
+                          while the other ones not being displayed could look grey.
+
+                      <DemoFunctions 
+                        content={
+                            featuredQueries
+                          } 
+                      />
+                    */}
 
 
-                  </Parallax>
+                    </Parallax>
+                  </Box>
+
+                  {/* This is the section that displays the entire IDE window.
+                      it includes both the code snippet and the tabs on top of the window
+                  */}
+                  <IDE queriesData={wrapper.queries} />
                 </Box>
 
-                {/* This is the section that displays the entire IDE window.
-                    it includes both the code snippet and the tabs on top of the window
-                */}
-                <IDE queriesData={wrapper.queries} />
-              </Box>
-
+              </Grid>
             </Grid>
-
-            {/* This section is used to display the name of the wrapper, a description of the wrapper,
-                additional copy to generate engagement, and a CTA button that takes users to docs of the
-                specified wrappert
-            */}
-            <Grid item xs={12} md={6}>
-
-              {/* exciting title for the section */}
-              <Typography
-                variant='h3'
-                color='textPrimary'
-                className={classes.title}
-              >
-                {aboutThisSection.title}
-              </Typography>
-
-              {/* description about the wrapper dev experience */}
-              <Typography
-                variant='body1'
-                color='textSecondary'
-                className={classes.description}
-              >
-                {aboutThisSection.description  }
-              </Typography>
-
-              {/* The name of the wrapper currently displayed */}
-              <Typography
-                variant='h4'
-                color='textPrimary'
-                className={classes.title}
-              >
-                {wrapper.wrapperName}
-              </Typography>
-
-              {/* CTA to get people to use the specific wrapper */}
-              <Button
-              // still dunno what to calll this
-              //className={classes.heroButton}
-              color='primary'
-              href={wrapper.docsLink}
-              target="_blank"
-              rel="noredirect"
-              endIcon={<KeyboardArrowRightOutlined />}
-              type='submit'
-              variant='contained'
-            >
-              wrap with it
-            </Button>
-
-
-              {/* Description of the wrapper being displayed */}
-              <Typography
-                variant='body1'
-                color='textSecondary'
-                className={classes.description}
-              >
-                {wrapper.description } 
-              </Typography>
-
-
-            </Grid>
-          </Grid>
+          </Box>
         )}
-
-      </Parallax>
     </Box>
   );
 };
