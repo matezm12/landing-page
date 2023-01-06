9dfc49902efdcf51036d6bc32e3744fe1b8a3a83
Colin Spence <udappia@gmail.com>
2023-01-06 15:06:12 -0700

    Update MetaverseBackground.tsx

--- components/MetaverseBackground.tsx
+++ components/MetaverseBackground.tsx
@@ -11,7 +11,7 @@ const MetaverseBackground = () => {
           border: "none",
           width: "100%",
           height: "100%",
-          opacity: 0.5,
+          opacity: 0.3,
         },
       }}
     >
